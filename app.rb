4ca8574be49170fb3120ab3eedef874fff53e5f2
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-11 12:00:32 -0500

    solution

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Victoria"
+  end
+
+  get '/hometown' do 
+    "My hometown is Charlottesville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Shake It Off"
+  end
 end
