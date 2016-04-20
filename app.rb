247a270c5afb82f4f606456fc8c1ab8a39f0cc54
Charles Pletcher <cpletcher2@gmail.com>
2016-04-20 16:29:36 -0400

    Remove solution from master
    Fixes #5

--- app.rb
+++ app.rb
@@ -1,15 +1,4 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/name' do
-    "My name is Victoria"
-  end
-
-  get '/hometown' do
-    "My hometown is Charlottesville"
-  end
-
-  get '/favorite-song' do
-    "My favorite song is Shake It Off"
-  end
 end
