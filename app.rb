1498ebc9f9f34bac7bd51391b637a85cb97cd537
Cedric David <cedric.david@btopenworld.com>
2017-05-16 07:48:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is __"
+  end
+   get '/hometown' do
+    "My hometown is __"
+  end
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
 end
