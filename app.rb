a23e727e80a99c1a04ddafc65ec6cd46240b421c
Cedric David <cedric.david@btopenworld.com>
2018-09-18 10:57:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+     "My name is C" 
+  end
+  get '/hometown' do
+    "My hometown is B"
+  end
+  get '/favorite-song' do
+    "My favorite song is N"
+  end
 end
